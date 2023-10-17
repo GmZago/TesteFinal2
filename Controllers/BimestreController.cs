ae21bb2d005055ee177ce295d2a3fe3375edc6d5
GmZago <gm.zago@hotmail.com>
2023-10-17 15:24:32 -0300

    Criação Controller e Views de Bimestre.

---
+++ Controllers/BimestreController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TesteFinal.Models;
+
+namespace TesteFinal.Controllers
+{
+    public class BimestreController : Controller
+    {
+        private readonly Contexto _context;
+
+        public BimestreController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Bimestre
+        public async Task<IActionResult> Index()
+        {
+              return _context.Bimestre != null ? 
+                          View(await _context.Bimestre.ToListAsync()) :
+                          Problem("Entity set 'Contexto.Bimestre'  is null.");
+        }
+
+        // GET: Bimestre/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Bimestre == null)
+            {
+                return NotFound();
+            }
+
+            var bimestre = await _context.Bimestre
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bimestre == null)
+            {
+                return NotFound();
+            }
+
+            return View(bimestre);
+        }
+
+        // GET: Bimestre/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bimestre/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,BimestreDescricao")] Bimestre bimestre)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bimestre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bimestre);
+        }
+
+        // GET: Bimestre/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Bimestre == null)
+            {
+                return NotFound();
+            }
+
+            var bimestre = await _context.Bimestre.FindAsync(id);
+            if (bimestre == null)
+            {
+                return NotFound();
+            }
+            return View(bimestre);
+        }
+
+        // POST: Bimestre/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BimestreDescricao")] Bimestre bimestre)
+        {
+            if (id != bimestre.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bimestre);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BimestreExists(bimestre.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bimestre);
+        }
+
+        // GET: Bimestre/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Bimestre == null)
+            {
+                return NotFound();
+            }
+
+            var bimestre = await _context.Bimestre
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bimestre == null)
+            {
+                return NotFound();
+            }
+
+            return View(bimestre);
+        }
+
+        // POST: Bimestre/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Bimestre == null)
+            {
+                return Problem("Entity set 'Contexto.Bimestre'  is null.");
+            }
+            var bimestre = await _context.Bimestre.FindAsync(id);
+            if (bimestre != null)
+            {
+                _context.Bimestre.Remove(bimestre);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BimestreExists(int id)
+        {
+          return (_context.Bimestre?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
